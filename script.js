92fcd446df41c663fbbfa787eb75fecf59b6c8b6
auswald <auswaldyk25@gmail.com>
2026-04-18 17:47:46 +0800

    Improved UI

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 const today = new Date();
-const todayString = today.toLocaleDateString([], {day: 'numeric'});
+const todayString = today.toLocaleDateString([], {
+  day: "numeric",
+  month: "long",
+});
 const currentTime = new Date();
-const currentTimeString = currentTime.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'});
+const currentTimeString = currentTime.toLocaleTimeString([], {
+  hour: "2-digit",
+  minute: "2-digit",
+});
 document.getElementById("dayDisplay").textContent = todayString;
 document.getElementById("timeDisplay").textContent = currentTimeString;
 async function fetchWeather(mountain) {
@@ -14,39 +20,45 @@ async function fetchWeather(mountain) {
     }
     const data = await response.json();
     console.log(data);
-    displayWeather(data, mountain)
+    displayWeather(data, mountain);
   } catch (error) {
     console.log("An error occurred", error.message);
     return null;
   }
 }
 
-function capitalizeFirstChar(str){
-    return str
-          .split(" ")
-          .map(char => {
-            if(char.length === 0) return char;
-            return char[0].toUpperCase() + char.slice(1).toLowerCase();
-          }).join(" ");
+function capitalizeFirstChar(str) {
+  return str
+    .split(" ")
+    .map((char) => {
+      if (char.length === 0) return char;
+      return char[0].toUpperCase() + char.slice(1).toLowerCase();
+    })
+    .join(" ");
 }
 
 function displayWeather(data, mountain) {
   document.getElementById("location").textContent = `${mountain.location}`;
+  document.getElementById("difficultyValue").textContent =
+    `${mountain.difficulty}`;
   document.getElementById("mountain").textContent = `${mountain.name}`;
   document.getElementById("temperature").textContent = `${data.main.temp}`;
   document.getElementById("feelsLike").textContent = `${data.main.feels_like}`;
-  document.getElementById("windValue").textContent =
-    `${data.wind.speed}`;
-  document.getElementById("elevationValue").innerHTML =
-    `${mountain.elevation}`;
-    const cloudiness = capitalizeFirstChar(data.weather[0].description);
-  document.getElementById("cloudinessValue").textContent =
-    cloudiness;
-  const mountainSunrise = new Date(data.sys.sunrise * 1000)
-  const sunriseTimeString = mountainSunrise.toLocaleTimeString([], {hour: '2-digit', minute: '2-digit'})  
+  document.getElementById("windValue").textContent = `${data.wind.speed}`;
+  document.getElementById("elevationValue").innerHTML = `${mountain.elevation}`;
+  const cloudiness = capitalizeFirstChar(data.weather[0].description);
+  document.getElementById("cloudinessValue").textContent = cloudiness;
+  const mountainSunrise = new Date(data.sys.sunrise * 1000);
+  const sunriseTimeString = mountainSunrise.toLocaleTimeString([], {
+    hour: "2-digit",
+    minute: "2-digit",
+  });
   document.getElementById("sunriseValue").textContent = sunriseTimeString;
   const mountainSunset = new Date(data.sys.sunset * 1000);
-  const sunsetTimeString = mountainSunset.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'});
+  const sunsetTimeString = mountainSunset.toLocaleTimeString([], {
+    hour: "2-digit",
+    minute: "2-digit",
+  });
   document.getElementById("sunsetValue").textContent = sunsetTimeString;
 }
 
@@ -57,7 +69,8 @@ const mountains = [
     lat: 16.5975,
     lon: 120.8992,
     elevation: "2,922 - 2,930 ",
-    location: "Benguet, Kabayan"
+    location: "Benguet, Kabayan",
+    difficulty: "3/9 - 7/9", // Varies by trail
   },
   {
     id: "ugo",
@@ -65,7 +78,8 @@ const mountains = [
     lat: 16.3192,
     lon: 120.802,
     elevation: "2,150 - 2,220",
-    location: "Benguet, Itogon"
+    location: "Benguet, Itogon",
+    difficulty: "5/9",
   },
   {
     id: "maculot",
@@ -73,7 +87,8 @@ const mountains = [
     lat: 13.9212,
     lon: 121.0519,
     elevation: "930 - 957",
-    location: "Batangas, Cuenca"
+    location: "Batangas, Cuenca",
+    difficulty: "3/9 - 4/9", // Varies by route
   },
   {
     id: "tapulao",
@@ -81,7 +96,8 @@ const mountains = [
     lat: 15.481,
     lon: 120.1213,
     elevation: "2,034 - 2,037",
-    location: "Zambales, Palauig"
+    location: "Zambales, Palauig",
+    difficulty: "5/9",
   },
   {
     id: "arayat",
@@ -89,7 +105,8 @@ const mountains = [
     lat: 15.20129,
     lon: 120.74312,
     elevation: "1,026",
-    location: "Pampanga, Arayat"
+    location: "Pampanga, Arayat",
+    difficulty: "5/9 - 7/9", // Varies by route
   },
   {
     id: "batulao",
@@ -97,7 +114,8 @@ const mountains = [
     lat: 14.0403,
     lon: 120.802,
     elevation: "693 - 811",
-    location: "Batangas, Nasugbu"
+    location: "Batangas, Nasugbu",
+    difficulty: "4/9",
   },
   {
     id: "daraitan",
@@ -105,7 +123,8 @@ const mountains = [
     lat: 14.6131,
     lon: 121.4388,
     elevation: "719 - 739",
-    location: "Rizal, Tanay"
+    location: "Rizal, Tanay",
+    difficulty: "4/9",
   },
   {
     id: "pinatubo",
@@ -113,7 +132,8 @@ const mountains = [
     lat: 15.14167,
     lon: 120.35,
     elevation: "1,486",
-    location: "Tarlac, Capas"
+    location: "Tarlac, Capas",
+    difficulty: "2/9",
   },
   {
     id: "marami",
@@ -121,7 +141,8 @@ const mountains = [
     lat: 14.1986,
     lon: 120.6861,
     elevation: "633",
-    location: "Cavite, Maragondon"
+    location: "Cavite, Maragondon",
+    difficulty: "3/9",
   },
   {
     id: "talamitam",
@@ -129,7 +150,8 @@ const mountains = [
     lat: 14.10781,
     lon: 120.75991,
     elevation: "630 - 704",
-    location: "Batangas, Nasugbu"
+    location: "Batangas, Nasugbu",
+    difficulty: "2/9",
   },
 ];
 
