f78eedd51150230f2a5ebd8a59d46329a6b68b60
auswald <auswaldyk25@gmail.com>
2026-04-18 23:35:20 +0800

    Added Difficulty Description, added distance metric

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const todayString = today.toLocaleDateString([], {
   day: "numeric",
   month: "long",
   day: "2-digit",
-  year: "numeric"
+  year: "numeric",
 });
 const currentTime = new Date();
 const currentTimeString = currentTime.toLocaleTimeString([], {
@@ -39,17 +39,103 @@ function capitalizeFirstChar(str) {
     .join(" ");
 }
 
+function getHumidity(humidityScore) {
+  switch (true) {
+    case humidityScore < 30:
+      return "Very Dry";
+      break;
+    case humidityScore >= 30 && humidityScore < 49:
+      return "Dry";
+      break;
+    case humidityScore >= 50 && humidityScore < 70:
+      return "Normal";
+      break;
+    case humidityScore >= 70 && humidityScore < 85:
+      return "Humid";
+      break;
+    case humidityScore >= 85 && humidityScore < 95:
+      return "Very Humid";
+      break;
+    default:
+      return "Foggy";
+  }
+}
+
+function humidityAdvice(humidity) {
+  switch (humidity) {
+    case "Very Dry":
+      return "Extremely dry air. High fire risk on open slopes. Drink water frequently.You're losing moisture faster than you realize. Lip balm and moisturizer strongly recommended.";
+      break;
+    case "Dry":
+      return "Crisp and pleasant. Ideal hiking weather! Sweat evaporates quickly keeping you cool. Still remember to hydrate regularly, dry air masks dehydration.";
+      break;
+    case "Normal":
+      return "Typical forest humidity. Comfortable for most hikers. Trail conditions should be normal. Light mist possible near summits.";
+      break;
+    case "Humid":
+      return "Sticky and damp. Sweat won't evaporate well, you'll feel warmer than actual temperature. Trails may be slippery. Wear moisture wicking clothing.";
+      break;
+    case "Very Humid":
+      return "Heavy air, likely foggy. Expect limited visibility and possible sea of clouds. Jacket and gear will stay wet. Protect electronics from moisture.";
+      break;
+    default:
+      return "Dense fog and drizzle. Visibility severely reduced. Stay on marked trails. Risk of hypothermia even in moderate temperatures. Waterproof layers essential.";
+  }
+}
+
+function getDifficulty(difficulty) {
+  switch (difficulty) {
+    case 1:
+      return "very Easy";
+      break;
+    case 2:
+      return "Easy";
+      break;
+    case 3:
+      return "Mild";
+      break;
+    case 4:
+      return "Average";
+      break;
+    case 5:
+      return "Moderate";
+      break;
+    case 6:
+      return "Challenging";
+      break;
+    case 7:
+      return "Difficult";
+      break;
+    case 8:
+      return "Strenuous";
+      break;
+    case 9:
+      return "Technical";
+      break;
+    default:
+      return "Could not get difficulty";
+  }
+}
+
 function displayWeather(data, mountain) {
   document.getElementById("location").textContent = `${mountain.location}`;
-  document.getElementById("difficultyValue").textContent =
-    `${mountain.difficulty}`;
+  const mountainDifficulty = mountain.difficulty;
+  const difficultyValue = document.getElementById("difficultyValue");
+  difficultyValue.textContent = `${mountain.difficulty}/9`;
+  document.getElementById("hikabilityDescription").textContent =
+    getDifficulty(mountainDifficulty);
   document.getElementById("mountain").textContent = `${mountain.name}`;
-  document.getElementById("humidityValue").textContent =
-    `${data.main.humidity}`;
+  const humidityScore = data.main.humidity;
+  const humidityCondition = getHumidity(data.main.humidity);
+  document.getElementById("humidityValue").textContent = `${humidityScore}%`;
+  document.getElementById("humidityResult").textContent =
+    getHumidity(humidityScore);
+  document.getElementById("humidityDescription").textContent =
+    humidityAdvice(humidityCondition);
   document.getElementById("temperature").textContent = `${data.main.temp}`;
   document.getElementById("feelsLike").textContent = `${data.main.feels_like}`;
   document.getElementById("windValue").textContent = `${data.wind.speed}`;
-  document.getElementById("elevationValue").innerHTML = `${mountain.elevation}`;
+  document.getElementById("elevationValue").innerHTML = `${mountain.elevation}m`;
   const cloudiness = capitalizeFirstChar(data.weather[0].description);
   document.getElementById("cloudinessValue").textContent = cloudiness;
   const mountainSunrise = new Date(data.sys.sunrise * 1000);
@@ -72,36 +158,36 @@ const mountains = [
     name: "Mt. Pulag",
     lat: 16.5975,
     lon: 120.8992,
-    elevation: "2,922 - 2,930 ",
+    elevation: "2,922",
     location: "Benguet, Kabayan",
-    difficulty: "3/9 - 7/9", // Varies by trail
+    difficulty: 3, // Varies by trail
   },
   {
     id: "ugo",
     name: "Mt. Ugo",
     lat: 16.3192,
     lon: 120.802,
-    elevation: "2,150 - 2,220",
+    elevation: "2,150",
     location: "Benguet, Itogon",
-    difficulty: "5/9",
+    difficulty: 5,
   },
   {
     id: "maculot",
     name: "Mt. Maculot",
     lat: 13.9212,
     lon: 121.0519,
-    elevation: "930 - 957",
+    elevation: "930",
     location: "Batangas, Cuenca",
-    difficulty: "3/9 - 4/9", // Varies by route
+    difficulty: 3, // Varies by route
   },
   {
     id: "tapulao",
     name: "Mt. Tapulao",
     lat: 15.481,
     lon: 120.1213,
-    elevation: "2,034 - 2,037",
+    elevation: "2,034",
     location: "Zambales, Palauig",
-    difficulty: "5/9",
+    difficulty: 5,
   },
   {
     id: "arayat",
@@ -110,25 +196,25 @@ const mountains = [
     lon: 120.74312,
     elevation: "1,026",
     location: "Pampanga, Arayat",
-    difficulty: "5/9 - 7/9", // Varies by route
+    difficulty: 5, // Varies by route
   },
   {
     id: "batulao",
     name: "Mt. Batulao",
     lat: 14.0403,
     lon: 120.802,
-    elevation: "693 - 811",
+    elevation: "693",
     location: "Batangas, Nasugbu",
-    difficulty: "4/9",
+    difficulty: 4,
   },
   {
     id: "daraitan",
     name: "Mt. Daraitan",
     lat: 14.6131,
     lon: 121.4388,
-    elevation: "719 - 739",
+    elevation: "719",
     location: "Rizal, Tanay",
-    difficulty: "4/9",
+    difficulty: 4,
   },
   {
     id: "pinatubo",
@@ -137,7 +223,7 @@ const mountains = [
     lon: 120.35,
     elevation: "1,486",
     location: "Tarlac, Capas",
-    difficulty: "2/9",
+    difficulty: 2,
   },
   {
     id: "marami",
@@ -146,16 +232,16 @@ const mountains = [
     lon: 120.6861,
     elevation: "633",
     location: "Cavite, Maragondon",
-    difficulty: "3/9",
+    difficulty: 3,
   },
   {
     id: "talamitam",
     name: "Mt. Talamitam",
     lat: 14.10781,
     lon: 120.75991,
-    elevation: "630 - 704",
+    elevation: "630",
     location: "Batangas, Nasugbu",
-    difficulty: "2/9",
+    difficulty: 2,
   },
 ];
 
